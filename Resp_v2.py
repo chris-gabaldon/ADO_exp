0b2e5b1697f1b14d7c946976152aaa020c7e20d8
Christopher Gabaldon <129426874+chris-gabaldon@users.noreply.github.com>
2024-06-27 10:14:06 -0300

    Resp_v2.py

--- Resp_v2.py
+++ Resp_v2.py
@@ -5,19 +5,19 @@ como el modelo usando la funcion exp.update
 """
 import pandas as pd
 
-df = pd.DataFrame(columns=['Nombre', 'Edad','Trial', 'Diseño', 'Resultado','Numeros','Acierto'])
+df = pd.DataFrame(columns=['Trial', 'Diseño', 'Resultado','Numeros','Acierto'])
 
 
-def agregar_datos(nombre, edad, diseño, resultado, numeros, aciertos):
+def agregar_datos( diseño, resultado, numeros, aciertos):
     global df
-    new_entry = pd.DataFrame({'Nombre': [nombre], 'Edad': [edad], 'Diseño': [diseño], 'Resultado': [resultado], 'Numeros': [numeros], 'Acierto': [aciertos]})
+    new_entry = pd.DataFrame({ 'Diseño': [diseño], 'Resultado': [resultado], 'Numeros': [numeros], 'Acierto': [aciertos]})
     df = pd.concat([df, new_entry], ignore_index=True)
     df['Trial'] = range(1, len(df) + 1) 
 
-def process_response(exp, d1, respuesta, nombre, edad,numeros):
+def process_response(exp, d1, respuesta,numeros):
     aciertos=0
     prom_fijo=50
-    d=d = [41, 44, 46, 48, 49, 51, 52, 54, 56, 59]
+    d= [41, 44, 46, 48, 49, 51, 52, 54, 56, 59]
     if respuesta == ['left'] and d[d1] < prom_fijo:
         aciertos += 1
     if respuesta == ['right'] and d[d1] > prom_fijo:
@@ -25,6 +25,7 @@ def process_response(exp, d1, respuesta, nombre, edad,numeros):
 
     y = 1 if respuesta == ['left'] else 0
     exp.update(d1, y)
-    agregar_datos(nombre, edad, d[d1], y,numeros,aciertos)
+    agregar_datos(d[d1], y,numeros,aciertos)
+    
     
     
